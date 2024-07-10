c033c04054255afc2ed47022b065f765167122c9
Daniil <EaseJeat1999_1004@mail.ru>
2024-07-10 21:08:43 +0300

    Исправил ошибки

--- price_sync/core/models.py
+++ price_sync/core/models.py
@@ -2,27 +2,41 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    path_segment = models.CharField(max_length=500)
-
-    def __str__(self):
-        return self.name
-
-
 class Site(models.Model):
     name = models.CharField(max_length=255)
-    url = models.CharField(max_length=500)
+    domain = models.URLField(max_length=500, null=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Site'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products', null=True)
+    resource_path = models.CharField(max_length=500)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='products', null=True)
+
+    @property
+    def full_url(self):
+        return f"{self.site.domain}/{self.resource_path}"
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
 
 
 class PriceHistory(models.Model):
     price = models.FloatField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='price_history', null=True)
     date = models.DateTimeField()
 
     def __str__(self):
         return f"{self.product.name} {self.price} {self.date}"
+
+    class Meta:
+        verbose_name = 'PriceHistory'
